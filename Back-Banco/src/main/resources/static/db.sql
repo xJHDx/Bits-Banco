d52e3d31cae9fdd5270e393a90c4a4689438413b
Johnson Torres <44690752+xJHDx@users.noreply.github.com>
2022-07-23 01:17:27 -0500

    sql

--- Back-Banco/src/main/resources/static/db.sql
+++ Back-Banco/src/main/resources/static/db.sql
@@ -16,26 +16,38 @@ CREATE TABLE IF NOT EXISTS  `cliente`(
 	id_cliente int PRIMARY KEY NOT NULL AUTO_INCREMENT,
 	nombre_cliente varchar(100) NOT NULL,
 	direccion_cliente varchar(100) NOT NULL,
-	telefono_cliente bigint(100) NOT NULL
+	telefono_cliente bigint(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 
 CREATE TABLE IF NOT EXISTS  `cuenta`(
 	id_cuenta int PRIMARY KEY NOT NULL AUTO_INCREMENT,
-	id_cliente int NOT NULL,
+	cliente_ibfk int NOT NULL,
 	numero_cuenta bigint(100) NOT NULL,
-	saldo_cuenta bigint(100) NOT NULL,
-	INDEX (id_cliente),
-    FOREIGN KEY (id_cliente) REFERENCES cliente(id_cliente)
+	saldo_cuenta bigint(30) NOT NULL,
+	INDEX (cliente_ibfk),
+    CONSTRAINT `cliente_ibfk` FOREIGN KEY (cliente_ibfk) REFERENCES `cliente`(id_cliente)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 
 CREATE TABLE IF NOT EXISTS  `movimiento`(
 	id_movimiento int PRIMARY KEY NOT NULL AUTO_INCREMENT,
-	id_cuenta int NOT NULL,
+	cuenta_ibfk int NOT NULL,
 	tipo_movimiento varchar(100)NOT NULL,
 	fecha_movimiento DATE,
 	valor_movimiento bigint(100),
-	INDEX (id_cuenta),
-    FOREIGN KEY (id_cuenta) REFERENCES cuenta(id_cuenta)
+	INDEX (cuenta_ibfk),
+    CONSTRAINT `cuenta_ibfk` FOREIGN KEY (cuenta_ibfk) REFERENCES `cuenta`(id_cuenta)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--  Info
+INSERT INTO  `cliente`(id_cliente,nombre_cliente,direccion_cliente,telefono_cliente)
+	VALUES (1,'johnson','Ibague',310261644);
+
+
+INSERT INTO `cuenta`(cliente_ibfk,numero_cuenta,saldo_cuenta)
+	VALUES (1,101,500000);
+
+
+INSERT INTO  `movimiento` (cuenta_ibfk,tipo_movimiento,fecha_movimiento,valor_movimiento)
+	VALUES (1,'debito','2022-07-22',500000);
