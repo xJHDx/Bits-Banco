8d298f3748130405ecbded4645772340dee6a3ce
Johnson Torres <44690752+xJHDx@users.noreply.github.com>
2022-07-25 21:53:34 -0500

    notas chucknorris

--- Front-Banco/src/app/components/header/header.component.ts
+++ Front-Banco/src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ChucknorrisServices } from 'src/app/services/chucknorrisServices';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  notas:string = "";
+
+  constructor(private chucknorrisServices:ChucknorrisServices) { }
 
   ngOnInit(): void {
+    this.chucknorrisServices.getChucknorris().subscribe(r=>{
+      this.notas = r.value;
+    })
   }
 
 }
